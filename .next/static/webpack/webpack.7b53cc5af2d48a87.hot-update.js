a2593ba01ec179f77f04cf4f2c14ff02eab9c024
MouseMiggy <francocaballero868@gmail.com>
2025-10-23 15:24:12 +0800

    for testing vercel with server

--- .next/static/webpack/webpack.7b53cc5af2d48a87.hot-update.js
+++ .next/static/webpack/webpack.7b53cc5af2d48a87.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "18e121f33ffc30d4"; }
+/******/ 	__webpack_require__.h = function() { return "93019d612a235ab0"; }
 /******/ }();
 /******/ 
 /******/ }
